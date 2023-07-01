74609e21d2d393f05031dae89342ef8f87f5e147
Bata Botond <atabboti@gmail.com>
2023-07-01 10:50:35 +0200

    add header and a searchbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
+import Header from './components/Header'
+import SearchBar from './components/SearchBar'
 
 function App() {
 
   return (
-    <>
-      hello
-    </>
+    <div >
+      <Header/>
+      <SearchBar/>
+    </div>
   )
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>K2 translate</div>
+  )
+}
+
+export default Header
