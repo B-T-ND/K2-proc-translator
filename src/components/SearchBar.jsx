8440694e0a38440cc7f06a6865d16cea05600f19
Bata Botond <atabboti@gmail.com>
2023-07-02 16:03:19 +0200

    add the answer panel

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,17 +1,22 @@
 import React, { useState } from 'react'
 import LanguageDropdown from './LanguageDropdown'
 import "../css/SearchBar.css"
+import Answer from './Answer'
 
 const SearchBar = () => {
   const [translateFrom, setTranslateFrom] = useState("")
   const [translateTo, setTranslateTo] = useState("")
+  const handleGoButtonPress = () => {
+
+  }
 
   return (
     <div className='searchbar-container'>
       <input className='searchbar' type='text' placeholder='Search...'></input>
       <LanguageDropdown title={"Translate from:"} selectedLanguage={translateFrom} setSelectedLanguage={setTranslateFrom} />
       <LanguageDropdown title={"Translate to:"} selectedLanguage={translateTo} setSelectedLanguage={setTranslateTo} />
-      <button className='button-4' role="button">Go</button>
+      <button className='button-4' role="button" onClick={handleGoButtonPress}>Go</button>
+      <Answer />
     </div>
   )
 }
